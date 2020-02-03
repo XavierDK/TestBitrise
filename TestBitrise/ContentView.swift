0db37140562bed8395a407020dfdbe70681e2025
Xavier De Koninck <xdekoninck@Traveldoo.com>
2020-02-03 11:29:42 +0100

    First version

---
+++ TestBitrise/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestBitrise
+//
+//  Created by Xavier De Koninck on 03/02/2020.
+//  Copyright © 2020 Traveldoo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
